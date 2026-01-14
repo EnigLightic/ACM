94354e578407d63e4f3567609446a0f0416e116d
EnigLightic <m19967821793@163.com>
2026-01-14 23:38:00 +0800

    1072D3 practice

---
+++ 1072D3/a.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#include<climits>
+#include<vector>
+#include<stack>
+#include<map>
+#include<list>
+#include<queue>
+#include<deque>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+
+using namespace std;
+#define int long long
+#define ll long long
+#define vi vector<int> 
+#define pb push_back 
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define endl '\n'
+
+[[maybe_unused]] const int INF = 1e18;
+[[maybe_unused]] const int N   = 2e5 + 5;
+[[maybe_unused]] const int MOD = 1e9 + 7;
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n<=3) cout<<n<<endl;
+    else cout<<n%2<<endl;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int T=1;
+    cin>>T;
+    while(T--)
+        solve();
+    return 0;
+}
